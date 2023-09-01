36dd15c2899e60696fd54574b790cb91c16821d2
filipe alves <filipe_alvesoliveira@hotmail.com>
2023-09-01 14:26:06 -0300

    Study React - useState

---
+++ REACT/useState.jsx
@@ -0,0 +1,26 @@
+//utilizado para gerenciar estado
+//quando a varivel nao muda o valor não re-renderiza é qdo não utiliza o useState
+//sempre que re-renderiza teremos a impressão do novo valor = 25, incial = 15
+
+import { useState } from "react";
+
+const ManageData = () => {
+  let someData = 10;
+
+  const [number, setNumber] = useState(15); //desestruturar como array
+
+  return (
+    <div>
+      <div>
+        <p>Valor: {someData}</p>
+        <button onClick={() => (someData = 15)}>Mudar Variável</button>
+      </div>
+      <div>
+        <p>Valor: {number}</p>
+        <button onClick={() => setNumber(25)}>Mudar o state</button>
+      </div>
+    </div>
+  );
+};
+
+export default ManageData;
