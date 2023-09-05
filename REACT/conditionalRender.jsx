8c2dfe0c053eed4c44e6575be70a698d64488f2b
filipe alves <filipe_alvesoliveira@hotmail.com>
2023-09-04 21:36:25 -0300

    ternary Operator

--- REACT/conditionalRender.jsx
+++ REACT/conditionalRender.jsx
@@ -1,15 +1,33 @@
 //qdo imprimimos uma parte do template baseado em uma condição com if
+//if ternário - condição? bloco1 : bloco2
 
 import React from 'react'
 
 const conditionalRender = () => {
     const[x] = useState(true);
 
+    const [name, setName] = useState("João");
+
   return <div>
+    <h1>Condicional Simples</h1>
     <h1>Isso será exibido??</h1>
     {x && <p>Se x for true, sim!</p>} 
     {/* é exibido, if simples apenas com && variavel concatenada com &&*/}
     {!x && <p>agora é falso.</p>}
+    
+    <h1>If ternário</h1>
+    {/* {name === "João" ? "sim" : "não"} */} 
+    {name === "João" ? (
+        <div>
+            <p>O nome é João.</p>
+        </div>
+    ) : (
+        <div>
+            <p>Nome não econtrado!</p>
+        </div>
+    )}
+    {/* re-renderização do componente. */}
+    <button onClick={() => setName("José")}>Clique aqui</button>
   </div>
 }
 
