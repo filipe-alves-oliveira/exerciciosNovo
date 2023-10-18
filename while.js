760347afef8e9e7acea8c539dfbfa3efaa76c406
filipe alves <filipe_alvesoliveira@hotmail.com>
2023-10-18 15:55:10 -0300

    While

---
+++ while.js
@@ -0,0 +1,9 @@
+//Quando temos a necessidade de entrar em um looping com um valor indefinido.
+
+// ex.: um usuario deve comer até estar satisfeito, mas cada usuario come uma qtde diferente.
+
+// while (!user.full()) {
+//     user.eat(bread)
+// }
+
+// chamamos a função eat comer até a função full Cheio, retornar true, vdd, ai é negado pelo !parando o while.
