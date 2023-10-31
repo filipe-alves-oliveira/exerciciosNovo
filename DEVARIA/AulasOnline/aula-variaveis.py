dbe230f458a3effbad64b3a0bf6da80b3df3e534
filipe alves <filipe_alvesoliveira@hotmail.com>
2023-10-31 10:55:46 -0300

    classes with rodrigo mosken python

--- DEVARIA/AulasOnline/aula-variaveis.py
+++ DEVARIA/AulasOnline/aula-variaveis.py
@@ -1,4 +1,21 @@
-variavel = 0
-variavel_float = '"string"'
-# '' - um char, apenas um character
-# "" - vários caracther, string.
+variavel_inteiro = 1
+variavel_float = 1.0
+variavel_char = 'c'
+variavel_string = "string"
+
+# print(variavel_inteiro)
+# print(variavel_float)
+# print(variavel_char)
+# print(variavel_string)
+
+a = input("a: ")
+b = input("b: ")
+
+c = a
+a = b
+b = c
+
+print("a: " + a)
+print("b: " + b)
+
+Prioridade: 
