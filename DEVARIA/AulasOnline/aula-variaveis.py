361d685f0d835355ed90ec85c7726949ae60cbed
filipe alves <filipe_alvesoliveira@hotmail.com>
2023-11-01 14:26:52 -0300

    classes with rodrigo mosken

--- DEVARIA/AulasOnline/aula-variaveis.py
+++ DEVARIA/AulasOnline/aula-variaveis.py
@@ -1,21 +1,46 @@
-variavel_inteiro = 1
-variavel_float = 1.0
-variavel_char = 'c'
-variavel_string = "string"
+# variavel_inteiro = 1
+# variavel_float = 1.0
+# variavel_char = 'c'
+# variavel_string = "string"
 
-# print(variavel_inteiro)
-# print(variavel_float)
-# print(variavel_char)
-# print(variavel_string)
+# # print(variavel_inteiro)
+# # print(variavel_float)
+# # print(variavel_char)
+# # print(variavel_string)
 
-a = input("a: ")
-b = input("b: ")
+# a = input("a: ")
+# b = input("b: ")
 
-c = a
-a = b
-b = c
+# # c = a
+# # a = b
+# # b = c
 
-print("a: " + a)
-print("b: " + b)
+# print("a: " + a)
+# print("b: " + b)
 
-Prioridade: 
+# # ---------------------------------------------------------
+# a = int(input("a: "))
+# b = int(input("b: "))
+
+# print(f"resultado: {a - b}")
+
+# ---------------------------------------------------------
+import math
+
+# a = int(input("a: "))
+# b = int(input("b: "))
+# c = int(input("c: "))
+
+a = 1
+b = 12
+c = -13
+
+calculo_delta = b ** 2 - 4 * a * c
+
+print(f"resultado de delta eh: {calculo_delta}")
+
+x1 = (- b + (calculo_delta ** 0.5)) / (2*a)
+x2 = (- b - (calculo_delta ** 0.5)) / (2*a)
+
+print(f"resultado x1: {x1}")
+print(f"resultado x2: {x2}")
