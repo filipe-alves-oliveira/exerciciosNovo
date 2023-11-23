2779b0b67ec1fa6a3716a1b63acef5b3ecb28aaf
filipe alves <filipe_alvesoliveira@hotmail.com>
2023-11-22 21:02:36 -0300

    operadores lógicos python

---
+++ DEVARIA/AulasOnline/exerciciosArray.py
@@ -0,0 +1,23 @@
+# Peça 3 numeros ao usuário e print na tela os 3 numeros com ordem invertida.
+
+# numero1 = int(input("escreva o primeiro numero: "))
+# numero2 = int(input("escreva o segundo numero: "))
+# numero3 = int(input("escreva o terceiro numero: "))
+
+# array = []
+# array.append(numero1)
+# array.append(numero2)
+# array.append(numero3)
+
+# array = [numero3, numero2, numero1] 
+
+# print(array) - [3, 2, 1]
+
+# ------------------------------------------------------------------------
+
+# Desduplicando nome duplicados de uma lista.
+
+# compras = ["sabao", "papel higienico", "arroz", "arroz", "pasta", "pasta"]
+# print(compras) - ['sabao', 'papel higienico', 'arroz', 'arroz', 'pasta', 'pasta']
+# compras = list(set(compras)) 
+# print(compras) - ['sabao', 'papel higienico', 'arroz', 'pasta']
